33ab644eca51b8185b68172bd3a07467096f02ab
axkxd <altermaks375@gmail.com>
2025-02-04 08:31:40 +0300

    init db and minor fixes

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,12 @@
 from sqlalchemy.ext.asyncio import (
     async_sessionmaker, create_async_engine, AsyncSession
 )
+from sqlalchemy.future import select
 from app.core.config import DB_URL
+from app.db.models import Base, User
+
+from passlib.context import CryptContext
+
 from typing import AsyncGenerator
 
 
@@ -16,3 +21,30 @@ AsyncSessionLocal = async_sessionmaker(
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+# Инициализация базы данных: создание таблиц, если их нет
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    # Проверка наличия администратора
+    async with AsyncSessionLocal() as session:
+        result = await session.execute(
+            select(User).where(User.username == "admin")
+        )
+        admin = result.scalars().first()
+
+        if not admin:
+            hashed_password = pwd_context.hash("adminpass")
+            new_admin = User(
+                username="admin", password=hashed_password, role="admin"
+            )
+            session.add(new_admin)
+            await session.commit()
+            print("Администратор успешно создан.")
+        else:
+            print("Администратор уже существует.")
--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import users, notes, auth
 from app.middleware.log_middleware import LoggingMiddleware
+from app.db.session import init_db, engine
+
+from contextlib import asynccontextmanager
 
 
-app = FastAPI(title="Notes API")
+# Контекстный менеджер для управления жизненным циклом приложения
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(title="Notes API", lifespan=lifespan)
+
 app.add_middleware(LoggingMiddleware)
 
 
